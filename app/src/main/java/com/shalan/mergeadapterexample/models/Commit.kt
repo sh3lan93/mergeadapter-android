decc48cd035aebec93213bbb8ec5e02242ed6ed7
Mohamed Shalan <mohamed.sh3lan_93@outlook.com>
2020-04-12 21:56:41 +0200

    refactore: remove user id from  commit model

--- app/src/main/java/com/shalan/mergeadapterexample/models/Commit.kt
+++ app/src/main/java/com/shalan/mergeadapterexample/models/Commit.kt
@@ -6,4 +6,4 @@ import com.squareup.moshi.JsonClass
  * Created by Mohamed Shalan on 4/12/20.
  */
 @JsonClass(generateAdapter = true)
-data class Commit(val id: String, val userId: String, val commit: String)
+data class Commit(val id: String, val commit: String)
